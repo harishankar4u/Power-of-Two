0aaaa8f79499cd627613f42882ab14e3b2654fea
Hari Shankar Singh <hari@lmg8.tech>
2025-08-16 10:20:11 +0530

    Add project files.

---
+++ Power of Two/Program.cs
@@ -0,0 +1,29 @@
+﻿public class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(IsPowerOfTwo(16)); // output true
+        Console.WriteLine(IsPowerOfTwo(17)); // output false
+        Console.WriteLine(IsPowerOfTwo(1)); // output true
+        Console.WriteLine(IsPowerOfTwo(10)); // output false
+    }
+    public static bool IsPowerOfTwo(int n)
+    {
+        if(n==0)
+        {
+            return false;
+        }
+        while(n!=1)
+        {
+            if(n%2!=0)
+            {
+                return false;
+            }
+            else
+            {
+                n = n / 2;
+            }
+        }
+        return true;
+    }
+}
